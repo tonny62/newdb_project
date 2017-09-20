559a59dfbb66b357f41decc53e0385d7728b46ba
MAC <mac@Parewaris-MacBook-Pro.local>
2017-09-20 19:46:13 +0700

    edit timetable

--- action_page.php
+++ action_page.php
@@ -1,7 +1,7 @@
 <?php
   $uid = $_GET['uname'];
   $password = $_GET['password'];
-  $url = "http://localhost:80/miniproject";
+  $url = "http://localhost:80/newdb_project";
   // TODO: connect to database
   if ($uid == "admin" AND $password == "admin") {
     header("Location: $url/dashboard.php");
--- timetable.php
+++ timetable.php
@@ -7,6 +7,19 @@
   </head>
   <body>
     <?php // TODO: code timetable ?>
+
+    <style>
+
+    table, th, td {
+        border: 1px solid black;
+    }
+    th {
+    padding: 15px;
+    margin-top: 50px;
+}
+    </style>
+
+
     <div id="header">
         <p id="header_text">iSchool : ITS351 Mini Project</p>
       </div>
@@ -17,7 +30,62 @@
       </div>
       <div class="clear"></div>
     <div id="container">
-      Class Timetable
+
+
+      <br><br><br><br>
+      <table style="width:60%" >
+
+        <tr style="">
+            <th>Day/Time</th>
+            <th>9.00-10.20</th>
+            <th>10.40-12.00</th>
+            <th>12.00-13.00</th>
+            <th>13.00-14.20</th>
+            <th>14.40-16.00</th>
+        </tr>
+        <tr>
+              <th>Mon</th>
+              <th>ITS336</th>
+              <th>ITS331</th>
+              <th> </th>
+              <th>ITS322</th>
+              <th>CSS321</th>
+          </tr>
+          <tr>
+                <th>Tue</th>
+                <th>ITS336</th>
+                <th>ITS331</th>
+                <th> </th>
+                <th>ITS322</th>
+                <th>CSS321</th>
+            </tr>
+            <tr>
+                  <th>Wed</th>
+                  <th>ITS336</th>
+                  <th>ITS331</th>
+                  <th> </th>
+                  <th>ITS322</th>
+                  <th>CSS321</th>
+              </tr>
+              <tr>
+                    <th>Thu</th>
+                    <th>ITS336</th>
+                    <th>ITS331</th>
+                    <th> </th>
+                    <th>ITS322</th>
+                    <th>CSS321</th>
+                </tr>
+                <tr>
+                      <th>Fri</th>
+                      <th>ITS336</th>
+                      <th>ITS331</th>
+                      <th> </th>
+                      <th>ITS322</th>
+                      <th>CSS321</th>
+                  </tr>
+
+      </table>
+
     </div>
   </body>
 </html>
